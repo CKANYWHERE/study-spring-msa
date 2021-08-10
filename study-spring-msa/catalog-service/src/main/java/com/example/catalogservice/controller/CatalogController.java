28a62e965d23f069607eeef195ec187c4be4fb0d
M <janu723@naver.com>
2021-08-10 22:04:57 +0900

    developed category service

---
+++ study-spring-msa/catalog-service/src/main/java/com/example/catalogservice/controller/CatalogController.java
@@ -0,0 +1,45 @@
+package com.example.catalogservice.controller;
+
+import com.example.catalogservice.jpa.CatalogEntity;
+import com.example.catalogservice.service.CatalogService;
+import com.example.catalogservice.vo.ResponseCatalog;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/catalog")
+public class CatalogController {
+    Environment env;
+    CatalogService catalogService;
+
+    @Autowired
+    public CatalogController(Environment env, CatalogService catalogService){
+        this.env = env;
+        this.catalogService = catalogService;
+    }
+    @GetMapping("health_check")
+    public String status(){
+        return String.format("Working on %s", env.getProperty("local.server.port"));
+    }
+
+    @GetMapping("/catalogs")
+    public ResponseEntity<List<ResponseCatalog>> getUsers() {
+        Iterable<CatalogEntity> userList = catalogService.getAllCatalogs();
+
+        List<ResponseCatalog> res = new ArrayList<>();
+
+        userList.forEach(v ->{
+            res.add(new ModelMapper().map(v, ResponseCatalog.class));
+        });
+
+        return ResponseEntity.status(HttpStatus.OK).body(res);
+    }
+}
