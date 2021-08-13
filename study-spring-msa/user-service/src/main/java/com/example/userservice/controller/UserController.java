00ce182d15d0f22f5db7c5877337b488fca30147
Min-Chang-Kyeoung <janu723@naver.com>
2021-08-13 17:54:33 +0900

    add spring cloud config server

--- study-spring-msa/user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ study-spring-msa/user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -37,7 +37,10 @@ public class UserController {
 
     @GetMapping("health_check")
     public String status(){
-        return String.format("Working on %s", env.getProperty("local.server.port"));
+        return String.format(
+                "Working on "+ env.getProperty("local.server.port")
+                + ", token secret=" + env.getProperty("token.secret")
+                + ", expiration=" + env.getProperty("token.expiration_time"));
     }
 
     @GetMapping("welcome")
