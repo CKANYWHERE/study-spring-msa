7f81407366fd59abe5459552fd4d0343bfa827fd
Min-Chang-Kyeoung <janu723@naver.com>
2021-08-13 14:19:42 +0900

    add jwt

--- study-spring-msa/user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ study-spring-msa/user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.example.userservice.dto.UserDto;
 import com.example.userservice.jpa.UserEntity;
 import com.example.userservice.jpa.UserRepository;
 import com.example.userservice.vo.ResponseOrder;
+import org.apache.tomcat.jni.Error;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -62,6 +63,17 @@ public class UserServiceImpl implements UserService {
     @Override
     public Iterable<UserEntity> getUserByAll() {
         return userRepository.findAll();
+    }
+
+    @Override
+    public UserDto getUserDetailsByEmail(String userName) {
+        UserEntity entity = userRepository.findByEmail(userName);
+
+        if(entity == null)
+            throw new UsernameNotFoundException(userName);
+
+        UserDto returnDto = new ModelMapper().map(entity, UserDto.class);
+        return returnDto;
     }
 
     @Override
